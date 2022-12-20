45c85c649780e6c70ef48624df73d9e40e5648b9
njartiana <48241768+njartiana@users.noreply.github.com>
2022-12-20 17:12:55 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        TestCalcule test = new TestCalcule() ;
-        test.test_add() ;
-        test.test_multiplication() ;
-        test.test_substract() ;
-        test.test_division() ;
+        TestCalcule test = new TestCalcule();
+        test.test_add();
+        test.test_multiplication();
+        test.test_substract();
+        test.test_division();
     }
 }
