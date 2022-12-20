72d1f410a06953b30764b2823e8d73f9d60d8739
njartiana <48241768+njartiana@users.noreply.github.com>
2022-12-20 17:15:42 +0300

    Update Main adding message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         TestCalcule test = new TestCalcule();
+        System.out.println("-----------------> Starting test <----------------------");
         test.test_add();
         test.test_multiplication();
         test.test_substract();
         test.test_division();
-        System.out.println("Test finished");
+        System.out.println("#################### Test finished ##################");
     }
 }
