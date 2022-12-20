d6fa54eb8040ab6e061ff0820ddce93038617aa9
njartiana <48241768+njartiana@users.noreply.github.com>
2022-12-20 17:17:06 +0300

    Update Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         TestCalcule test = new TestCalcule();
-        System.out.println("-----------------> Starting test <----------------------");
+        System.out.println("-----------------> Starting test <---------------------- ");
         test.test_add();
         test.test_multiplication();
         test.test_substract();
