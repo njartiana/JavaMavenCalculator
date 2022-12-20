7fb3bf152045ca4e3a3f1d812cd9541c815c595c
njartiana <48241768+njartiana@users.noreply.github.com>
2022-12-20 17:02:04 +0300

    Update TestClass et Main

--- src/main/java/org/example/TestCalcule.java
+++ src/main/java/org/example/TestCalcule.java
@@ -5,34 +5,34 @@ public class TestCalcule {
     BaseCalcule base = new BaseCalcule();
     public void test_add(){
         if (base.add(2 , 2) == 4){
-            System.out.println("Test addition passed");
+            System.out.println("Test addition passed") ;
         }
         else{
-            System.out.println("Error for function addition");
+            System.out.println("Error for function addition") ;
         }
     }
     public void test_substract(){
         if (base.substraction(4 , 2) == 2){
-            System.out.println("Test subtraction passed");
+            System.out.println("Test subtraction passed") ;
         }
         else{
-            System.out.println("Error for function subtraction");
+            System.out.println("Error for function subtraction") ;
         }
     }
     public void test_multiplication(){
         if (base.multiplication(4 , 2) == 8){
-            System.out.println("Test multiplication passed");
+            System.out.println("Test multiplication passed") ;
         }
         else{
-            System.out.println("Error for function multiplication");
+            System.out.println("Error for function multiplication") ;
         }
     }
     public void test_division(){
         if(base.divide(4 , 2) == 2){
-            System.out.println("Test division passed");
+            System.out.println("Test division passed") ;
         }
         else{
-            System.out.println("Error for function division");
+            System.out.println("Error for function division") ;
         }
     }
 }
